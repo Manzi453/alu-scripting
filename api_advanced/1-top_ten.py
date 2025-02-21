366621621702fc6440ce34ffa3d81dca3b341ecf
Manzi453 <i.manziyamu@alustudent.com>
2025-02-21 13:24:58 +0200

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,12 +5,16 @@ import requests
 
 def top_ten(subreddit):
     """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    
+    # Check if the request was successful
+    if response.status_code == 200:
+        posts = response.json()['data']['children']
+        for post in posts:
+            print(post['data']['title'])
+    else:
+        # Provide more helpful error information
+        print("Error fetching data: ", response.status_code)
         print(None)
-        return
-    posts = response.json()['data']['children']
-    for post in posts:
-        print(post['data']['title'])
