7b0c96d7b56e036060a94d6ff10405e2167b4c85
Manzi453 <i.manziyamu@alustudent.com>
2025-03-14 14:42:07 +0200

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,13 +4,15 @@ import requests
 
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    """ Prints the titles of the first 10 hot posts listed in a subreddit. """
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {'User-Agent': 'python:subreddit.hot.posts:v1.0 (by /u/Manzi453)'}  # Custom User-Agent
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        posts = response.json()['data']['children']
+        for post in posts:
+            print(post['data']['title'])
+    else:
         print(None)
-        return
-    posts = response.json()['data']['children']
-    for post in posts:
-        print(post['data']['title'])
